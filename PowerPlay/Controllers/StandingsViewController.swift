2b6992f6cab2e53a432c6dd49704bc795cba1894
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-01-04 02:37:18 +0200

    content for standings cell

--- PowerPlay/Controllers/StandingsViewController.swift
+++ PowerPlay/Controllers/StandingsViewController.swift
@@ -62,6 +62,13 @@ extension StandingsViewController: UITableViewDataSource, UITableViewDelegate{
         cell.nameLabel.text = data[indexPath.row].team?.name ?? ""
         cell.logoLink = data[indexPath.row].team?.logo ?? ""
         cell.rankLabel.text = "\(data[indexPath.row].rank ?? 0)"
+        cell.ageinstLAbel.text = "A \(data[indexPath.row].all?.goals?.against ?? 0)"
+        cell.drawLabel.text = "D \(data[indexPath.row].all?.draw ?? 0)"
+        cell.forLabel.text = "F \(data[indexPath.row].all?.goals?.forTeam ?? 0)"
+        cell.loseLabel.text = "L \(data[indexPath.row].all?.lose ?? 0)"
+        cell.goalsDiffLAbel.text = "GD \(data[indexPath.row].goalsDiff ?? 0)"
+        cell.playersLabel.text = "PL \(data[indexPath.row].all?.played ?? 0)"
+        cell.winLabel.text = "W \(data[indexPath.row].all?.win ?? 0)"
         return cell
     }
     
