d5c53106d05004128f2ad260343756992499b03e
VolberdWork <volodymyr.kolomyltsvev@businessgroup.ltd>
2023-01-09 17:17:30 +0200

    dont editable/selectable & DateFormatter

--- PowerPlay/Extensions/ChangeDateFormat.swift
+++ PowerPlay/Extensions/ChangeDateFormat.swift
@@ -5,10 +5,12 @@ import AudioToolbox
     func changeDateFormat(dateString: String, fromFormat: String, toFormat: String) ->String {
         let inputDateFormatter = DateFormatter()
         inputDateFormatter.dateFormat = fromFormat
+       
         let date = inputDateFormatter.date(from: dateString)
         
         let outputDateFormatter = DateFormatter()
         outputDateFormatter.dateFormat = toFormat
+        outputDateFormatter.locale = Locale(identifier: "en_US_POSIX")
         return outputDateFormatter.string(from: date!)
     }
 
